64b82558b87f38ea1dcd8ea017e9cfdf148492ce
aliulorent <aliulorent@gmail.com>
2024-03-06 08:13:20 -0500

    Added font families and defined projects colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        dark:"#060C17",
+        primary:"#104399",
+        text:"#E6ECF6",
+        secondary:"#8EB0EB"
+      },
+      fontFamily:{
+        'sniglet': ['sniglet'],
+        'lilita' : ['"Lilita One"']
+      }
+    },
   },
   plugins: [],
 }
