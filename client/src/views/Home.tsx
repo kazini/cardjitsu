2507531beecfb536be3a44392b1a9ab3c7d4efa1
aliulorent <aliulorent@gmail.com>
2024-03-06 12:06:44 -0500

    Moved create game setup to home component

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 function Home() {
   const [inputText, setInputText] = useState("");
   const navigate = useNavigate();
@@ -12,10 +12,16 @@ function Home() {
       navigate(`/room/${inputText}`);
     }
   }
+  const handleCreateGame = async () =>{
+    fetch("http://localhost:3000/createGame")
+      .then(res=>res.json())
+      .then(data=>navigate(`/room/${data.id}`));
+  }
+
   return (
       <div className="h-full flex flex-col gap-4 justify-center items-center font-lilita text-text text-4xl lg:text-6xl text-center">
         <h1 className="font-sniglet text-7xl lg:text-9xl p-4">CARD JITSU</h1>
-        <Link to={`/create`} className="bg-primary rounded-2xl p-4">CREATE GAME</Link>
+        <div onClick={handleCreateGame} className="bg-primary rounded-2xl p-4 cursor-pointer">CREATE GAME</div>
         <div onClick={handleJoin} className="bg-primary rounded-2xl p-4 w-[300px] lg:w-[500px] flex flex-col gap-2 justify-center items-center cursor-pointer">
           <h3>JOIN GAME</h3>
           <input className="text-secondary text-center rounded-lg w-full shadow-inner" value={inputText} onChange={handleInput} placeholder="Room ID"></input>
